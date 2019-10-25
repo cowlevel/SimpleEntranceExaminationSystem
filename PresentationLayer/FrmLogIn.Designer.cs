ffde8636d01b64ffdd2fa89947eb9a0dd53c96b4
cowlevel07 <cyrjillcanon07@gmail.com>
2019-10-26 02:44:54 +0800

    Updated login label

--- PresentationLayer/FrmLogIn.Designer.cs
+++ PresentationLayer/FrmLogIn.Designer.cs
@@ -133,7 +133,7 @@
             // label3
             // 
             this.label3.BackColor = System.Drawing.Color.Teal;
-            this.label3.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(2, 141);
             this.label3.Name = "label3";
